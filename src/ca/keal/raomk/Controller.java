7abaf1b9aea224318810277edd586462239ae1a3
Ryan Dancy <ryan@keal.ca>
2017-12-10 11:01:59 -0500

    Add Controller, main.css, make D/R boxes grow
    The domain and range text boxes now extend across the bottom of the
    screen. Add Controller, which controls it, and main.css, which styles
    it. Also change some formatting in main.fxml.

--- src/ca/keal/raomk/Controller.java
+++ src/ca/keal/raomk/Controller.java
@@ -1,7 +1,26 @@
 package ca.keal.raomk;
 
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+
 public class Controller {
     
+    @FXML private Canvas canvas;
+    @FXML private GridPane drgrid;
+    @FXML private TextField domainTextBox;
+    @FXML private TextField rangeTextBox;
     
+    @FXML
+    public void initialize() {
+        // Let the domain and range text boxes expand across the bottom of the screen
+        ColumnConstraints col1 = new ColumnConstraints();
+        ColumnConstraints col2 = new ColumnConstraints();
+        col2.setHgrow(Priority.ALWAYS);
+        drgrid.getColumnConstraints().addAll(col1, col2);
+    }
     
 }
