24ad2bc3df4f30e0f326dfd166b183960cf29e08
Ryan Dancy <ryan@keal.ca>
2017-12-17 13:12:55 -0500

    Make domain/range text boxes functional, style
    The fences are now updated when the user presses enter in the domain or
    range text boxes.
    
    Also there is now decorative styled text around the domain/range boxes,
    and they are styled to have a blue border usually, but a red border on
    error (signaled through the "error" pseudo-class).

--- src/ca/keal/raomk/Controller.java
+++ src/ca/keal/raomk/Controller.java
@@ -1,20 +1,23 @@
 package ca.keal.raomk;
 
 import javafx.beans.binding.DoubleBinding;
+import javafx.css.PseudoClass;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
+
+import java.util.function.Consumer;
 
 public class Controller {
     
     // padding between the canvases and the D/R grid
     private static final int CANVAS_DRGRID_PADDING = 20;
     
-    @FXML private Pane root;
+    // for signifying parsing errors on the text boxes
+    private static final PseudoClass ERROR_CLASS = PseudoClass.getPseudoClass("error");
     
     // Canvas layers
     @FXML private Canvas layerBg;
@@ -53,13 +56,37 @@ public class Controller {
         }
     }
     
+    @FXML
+    public void updateDomain() {
+        updateDR(domainTextBox, 'x', ranch::setDomain);
+    }
+    
+    @FXML
+    public void updateRange() {
+        updateDR(rangeTextBox, 'y', ranch::setRange);
+    }
+    
+    private void updateDR(TextField textBox, char var, Consumer<DomainRange> updater) {
+        try {
+            updater.accept(DomainRange.parse(textBox.getText(), var));
+            textBox.pseudoClassStateChanged(ERROR_CLASS, false);
+        } catch (ParseException | Interval.EqualBoundsException e) {
+            updater.accept(null);
+            if (!textBox.getText().isEmpty()) {
+                // the user cleared the text box, not an error
+                textBox.pseudoClassStateChanged(ERROR_CLASS, true);
+            }
+        }
+    }
+    
     // Fix expansion of nodes that can't be expanded via FXML
     private void setupExpandNodes() {
         // Let the domain and range text boxes expand across the bottom of the screen
         ColumnConstraints col1 = new ColumnConstraints();
         ColumnConstraints col2 = new ColumnConstraints();
-        col2.setHgrow(Priority.ALWAYS);
-        drgrid.getColumnConstraints().addAll(col1, col2);
+        ColumnConstraints col3 = new ColumnConstraints();
+        col3.setHgrow(Priority.ALWAYS);
+        drgrid.getColumnConstraints().addAll(col1, col2, col3);
     
         // Expand the canvas layers manually
         Canvas[] layers = {layerBg, layerCows, layerFences};
